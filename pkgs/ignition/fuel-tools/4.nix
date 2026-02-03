0dab860cec84c594032f290b73b24fe64ef84a9e
Seb <133339614+Sebagabones@users.noreply.github.com>
2026-02-03 12:28:08 +0800

    Update 4.nix

--- pkgs/ignition/fuel-tools/4.nix
+++ pkgs/ignition/fuel-tools/4.nix
@@ -6,7 +6,7 @@ callPackage ./. ({
     majorVersion = "4";
     version = "4.8.3";
     srcHash = "sha256-Fa/xKb5J37OM0p8HB+Iu1cA47BbPBYZkO4z8XhSB5oc=";
-    ignition-common = ignition.common3;
+    ignition-common = ignition.common4;
     ignition-msgs = ignition.msgs5;
     ignition-cmake = ignition.cmake2;
   }
